546f8737209171f336c34bcb7b5a1d35c42e509c
akbar-pekat <akbarpekat4@gmail.com>
2020-10-26 20:34:51 +0800

    Function Ipay Worked maybe

--- p/detail.js
+++ p/detail.js
@@ -22,6 +22,8 @@ $(document).ready(function() {
     for (var i in cartArray) {
       output += '<li><div class="group"><div class="name"><h3>'+cartArray[i].name+'</h3><div class="qyt"><p>Rp '+cartArray[i].price+'</p><span>&nbsp;x&nbsp;</span><input type="number" id="input" value="'+cartArray[i].count+'" readonly /><span>=&nbsp;Rp '+cartArray[i].total+'</span></div></div><div class="action"><button class="delete-item" data-name="'+cartArray[i].name+'">Hapus</button></div></div></li>';
     }
+    localStorage.setItem("cartArray", JSON.stringify(cartArray));
+
     $("#show-carth").html(output);
     $("#total-cart").html(shoppingCart.totalCartTax());
   }
@@ -58,5 +60,66 @@ $(document).ready(function() {
   });
 
   displayCart();
+  $("#bayarbtn").hide()
+  $("#konfirmbtn").click(function(event) {
+    setTimeout(function() {
+      $("#bayarbtn").show()
+      $("#konfirmbtn").hide()
+    }, 2000);
+    $("#konfirmbtn").text("Memproses...")
+    var storedcartArray = JSON.parse(localStorage.getItem("cartArray"));
+    console.log(storedcartArray);
 
+    var namecount = storedcartArray.map(function(val) {
+      return val.name+" ("+val.count+")";
+    }).join('\n');
+    var totalprice = $("#total-cart").text();
+
+    var formdata = new FormData();
+    formdata.append("key",
+      "E8347004-91FC-469C-BC23-43B6D8749267");
+    formdata.append("action",
+      "payment");
+    formdata.append("product",
+      namecount);
+    formdata.append("price",
+      totalprice);
+    formdata.append("quantity",
+      "1");
+    formdata.append("comments",
+      "Pembayaran Pesanan BoluBoks");
+    formdata.append("ureturn",
+      "https://boluboks.com/");
+    formdata.append("unotify",
+      "https://boluboks.com/");
+    formdata.append("ucancel",
+      "https://boluboks.com/");
+    formdata.append("format",
+      "xml");
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    fetch("https://my.ipaymu.com/payment.htm",
+      requestOptions)
+    .then(response => response.text())
+    .then(result => $("#responseipay").html(result))
+    .catch(error => console.log('error', error));
+
+    setTimeout(function() {
+      var s1 = $("#responseipay").text();
+      var s2 = s1.substring(0,
+        37);
+      $("#responseipay").html(s2);
+    },
+      2000);
+  });
+
+  $("#bayarbtn").click(function(event) {
+    var linksipay = "https://my.ipaymu.com/payment/"+$("#responseipay").text();
+    window.location.href = linksipay;
+  });
 });
