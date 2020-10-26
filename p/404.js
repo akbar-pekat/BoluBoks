ba80e18d5fb0abc322688eb805af95236f1c8c1a
akbar-pekat <akbarpekat4@gmail.com>
2020-10-26 22:09:04 +0800

    404 in root P

---
+++ p/404.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  var loading = $(".loading");
+
+  setTimeout(function() {
+    loading.fadeOut();
+  }, 2000);
+
+
+  $("#back").click(function(event) {
+    window.location.href = "menu.html";
+  });
+
+});
